d4cef58c5fadc68c8145f99894b2a01af833ff9c
jinnycho <jinnycho.eng@gmail.com>
2021-02-23 18:29:59 -0500

    Addd marked to src, Create posts.js that renders html

---
+++ src/posts.js
@@ -0,0 +1,22 @@
+const posthtml = (data) => `
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="${data.attributes.description}" />
+        <title>${data.attributes.title}</title>
+    </head>
+    <body>
+        <header>
+            <a href="/">Go back home</a>
+        </header>
+        <div class="content">
+                <h1>${data.attributes.title}</h1>
+            <p>${new Date(parseInt(data.attributes.date)).toDateString()}</p>
+            <hr />
+            ${data.body}
+        </div>
+    </body>
+</html>
+`;

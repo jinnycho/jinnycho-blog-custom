21de0338f4507ff54e9a8d20572678b6b2ee5f6c
jinnycho <jinnycho.eng@gmail.com>
2021-03-02 00:05:49 -0500

    Working index.js that returns info about the docs

--- src/index.js
+++ src/index.js
@@ -1,32 +1,14 @@
 const fs = require("fs");
+const fm = require("front-matter");
 const config = require("./config");
+const createPost = require("./posts.js");
 
 const posts = fs
   .readdirSync(config.dev.postsdir)
-  .map(post => post.slice(0, -3));
+  .map(post => post.slice(0, -3))
+  .map(post => createPost(post));
 
-const posthtml = (data) => `
-  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="description" content="${data.attributes.description}" />
-    <title>${data.attributes.title}</title>
-       </head>
-      <body>
- 11         <header>
- 12             <a href="/">Go back home</a>
- 13         </header>
- 14         <div class="content">
- 15                 <h1>${data.attributes.title}</h1>
- 16             <p>${new Date(parseInt(data.attributes.date)).toDateString()}</p>
- 17             <hr />
- 18             ${data.body}
- 19         </div>
- 20     </body>
- 21 </html>
-`;
+console.log(posts);
 
 const createPosts = (posts) => {
   posts.forEach(post => {
---
+++ src/posts.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const config = require("./config");
+const fm = require("front-matter");
+const marked = require("marked");
+
+const createPost = (postPath) => {
+  const data = fs.readFileSync(`${config.dev.postsdir}/${postPath}.md`, "utf8");
+  const content = fm(data);
+  content.body = marked(content.body);
+  content.path = postPath;
+  return content;
+};
+
+module.exports = createPost;

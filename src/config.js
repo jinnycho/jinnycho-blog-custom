6f42b7ea1a9390b5f04e8aed9943b46ec8054b7d
jinnycho <jinnycho.eng@gmail.com>
2021-03-06 21:06:47 -0500

    Add more persosnal info in config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,9 @@
 const config = {
+  blogName: "Jinny's Blog",
+  blogDescription: "Sharing my world to the public",
+  author: "Jinny Ch",
+  authorDescription: "Programmer who dreams",
+  authorGithub: "",
   dev: {
     postsdir: "./content",
     outdir: "./public"

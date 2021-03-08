f46357106c5bea6c21c708880c1a92f210b6201e
jinnycho <jinnycho.eng@gmail.com>
2021-03-07 23:09:59 -0500

    Add index.js and cors in package.json

--- index.js
+++ index.js
@@ -1,14 +1,55 @@
-const express = require("express");
+const fs = require('fs');
+const matter = require('gray-matter');
+
+const express = require('express');
 const app = express();
+const port = 3000;
 
 app.use(express.static('public'));
 
-let bodyParser = require("body-parser");
+// Parse incoming request bodies in a middleware before your handlers
+let bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const path = require("path");
+const cors = require('cors');
+app.use(cors());
 
-app.set("views", path.join(__dirname, "blog"));
-app.set("view engine", "ejs");
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'blog'));
+app.set('view engine', 'ejs');
+
+app.get("/", function(request, response) {
+  response.sendFile(__dirname + "/views/index.html");
+});
+
+app.get('/blog/:article', (req, res) => {
+  const file = matter.read(__dirname + '/blog/' + req.params.article + '.md');
+
+  console.log(file);
+
+  // convert markdown content to HTML
+  let md = require('markdown-it')();
+  let content = file.content;
+  let result = md.render(content);
+
+  res.render('index', {
+    post: result,
+    title: file.data.title,
+    description: file.data.description,
+    image: file.data.image
+  });
+});
+
+app.get('/blog', (req, res) => {
+  const posts = fs.readdirSync(__dirname + '/blog').filter(file => file.endsWith('.md'));
+  res.render('blog', {
+    posts: posts
+  });
+});
+
+const listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
 
